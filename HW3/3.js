b1d60444cb018399dc9db86130391846e2c3c993
jerry92916 <166776207+jerry92916@users.noreply.github.com>
2024-06-18 15:01:03 +0800

    Update 3.js

--- HW3/3.js
+++ HW3/3.js
@@ -1,7 +1,23 @@
 function isPrime(n) {
-    if (n <= 1) return false;
-    for (let i = 2; i <= Math.sqrt(n); i++) {
-        if (n % i === 0) return false;
+    if (n <= 1) {
+        return false;
     }
-    return true;
+    if (n === 2) {
+        return true;
+    }
+    if (n % 2 === 0) {
+        return false; 
+    }
+    for (let i = 3; i <= Math.sqrt(n); i += 2) {
+        if (n % i === 0) {
+            return false; 
+        }
+    }
+    return true; 
 }
+
+console.log(isPrime(2)); 
+console.log(isPrime(3));  
+console.log(isPrime(4));  
+console.log(isPrime(29)); 
+console.log(isPrime(30)); 
