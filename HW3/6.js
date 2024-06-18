70d79749f89c7af234a6b6d2d98a8c9179560192
jerry92916 <166776207+jerry92916@users.noreply.github.com>
2024-06-18 15:04:46 +0800

    Update 6.js

--- HW3/6.js
+++ HW3/6.js
@@ -1,5 +1,8 @@
 function matrixAdd(a, b) {
-    if (a.length !== b.length || a[0].length !== b[0].length) throw new Error("Matrices must be of same dimensions");
+    if (a.length !== b.length || a[0].length !== b[0].length) {
+        throw new Error("Matrices must be of the same dimensions");
+    }
+
     let result = [];
     for (let i = 0; i < a.length; i++) {
         let row = [];
@@ -10,3 +13,16 @@ function matrixAdd(a, b) {
     }
     return result;
 }
+
+let matrix1 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+let matrix2 = [
+    [9, 8, 7],
+    [6, 5, 4],
+    [3, 2, 1]
+];
+let sum = matrixAdd(matrix1, matrix2);
+console.log(sum);
