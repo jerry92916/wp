33f8bad45f59152b37cb9796e20d19b2cd729bb5
jerry92916 <166776207+jerry92916@users.noreply.github.com>
2024-06-18 15:03:29 +0800

    Update 5.js

--- HW3/5.js
+++ HW3/5.js
@@ -1,8 +1,16 @@
 function vectorAdd(a, b) {
-    if (a.length !== b.length) throw new Error("Vectors must be of same length");
+    if (a.length !== b.length) {
+        throw new Error("Vectors must be of the same length");
+    }
+
     let result = [];
     for (let i = 0; i < a.length; i++) {
         result.push(a[i] + b[i]);
     }
     return result;
 }
+
+let vector1 = [1, 2, 3];
+let vector2 = [4, 5, 6];
+let sum = vectorAdd(vector1, vector2);
+console.log(sum);
