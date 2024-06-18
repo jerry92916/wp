68f59bed1eaab6d6d8cb7f438df1b27b402a23d1
jerry92916 <166776207+jerry92916@users.noreply.github.com>
2024-06-18 14:59:34 +0800

    Update 2.js

--- HW3/2.js
+++ HW3/2.js
@@ -1,5 +1,8 @@
 function printOdd(n) {
-    for (let i = 1; i < n; i += 2) {
-        console.log(i);
+    for (let i = 1; i < n; i++) {
+        if (i % 2 !== 0) { 
+            console.log(i); 
+        }
     }
 }
+printOdd(10);
