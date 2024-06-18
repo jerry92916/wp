43f5b444c9b99de6ddb01813cc48cea48cca859d
jerry92916 <166776207+jerry92916@users.noreply.github.com>
2024-06-18 15:02:22 +0800

    Update 4.js

--- HW3/4.js
+++ HW3/4.js
@@ -1,9 +1,29 @@
+function isPrime(n) {
+    if (n <= 1) {
+        return false; 
+    }
+    if (n === 2) {
+        return true;
+    }
+    if (n % 2 === 0) {
+        return false; 
+    }
+    for (let i = 3; i <= Math.sqrt(n); i += 2) {
+        if (n % i === 0) {
+            return false; 
+        }
+    }
+    return true;
+}
+
 function sumPrime(n) {
     let sum = 0;
     for (let i = 2; i < n; i++) {
         if (isPrime(i)) {
-            sum += i;
+            sum += i; 
         }
     }
-    return sum;
+    console.log(sum); 
 }
+sumPrime(10); 
+sumPrime(20); 
